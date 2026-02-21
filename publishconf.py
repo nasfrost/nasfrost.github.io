944a2783fb5a4d2bd03ddec76edef68bae0f8d8b
nasfrost <nasrahman@protonmail.com>
2026-02-21 02:31:07 +0000

    fix SITEURL

--- publishconf.py
+++ publishconf.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = "https://github.com/nasfrost/nasfrost.github.io"
+SITEURL = "https://nasfrost.github.io"
 RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
